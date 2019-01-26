af8b09d3b488409d3acb9c318ac8bcc0eddc6ee1
qingzhi <qingzhi@hoosho.com>
2019-01-26 10:00:16 +0800

    IU-2018.3.3 <qingzhi@qingzhi-pc Create baseRefactoring.xml, vim_settings.xml, ide.general.xml, project.default.xml, diff.xml, Default copy.xml, lombok-plugin.xml, other.xml, terminal.xml, Default.xml, applicationLibraries.xml, find.xml, ui.lnf.xml, log_highlighting.xml, filetypes.xml, keymap.xml, plugin_ui.xml, colors.scheme.xml, databaseDrivers.xml, Default.xml, laf.xml, Class.java, jdk.table.xml, path.macros.xml, proxy.settings.xml, vcs.xml, editor.codeinsight.xml, grepConsole.xml, editor.xml, usageView.xml, markdown_navigator.shared.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,8 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+ * @author qingzhi
+ * @date ${YEAR}/${MONTH}/${DAY}
+ */
+public class ${NAME} {
+}
