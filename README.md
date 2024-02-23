3b1569abcd3bde6d98868ba288cf58945d87c206
qingzhi <qingzhi@hoosho.com>
2024-02-23 17:50:50 +0800

    IU-2022.2.5 <user@DESKTOP-VQG4EFT Overwrite remote git@github.com:yzbyzz/idea-setting.git to local

--- README.md
+++
@@ -1,3 +0,0 @@
-# idea-setting
-
-https://www.jetbrains.com/help/idea/sharing-your-ide-settings.html#settings-repository
--- fileTemplates/internal/Class.java
+++
@@ -1,8 +0,0 @@
-#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
-#parse("File Header.java")
-/**
- * @author qingzhi
- * @date ${YEAR}/${MONTH}/${DAY}
- */
-public class ${NAME} {
-}
